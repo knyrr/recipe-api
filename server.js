d80f8bfaa9d2efbb58bb5262181d298bebe5c01d
klillenb <kertlillenberk@gmail.com>
2023-05-11 21:05:58 +0300

    refactor, moved quote to server

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
 
-require('dotenv').config();
+const recipesRouter = require("./routes/recipe");
+const quoteController = require("./controllers/quoteController");
+
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,24 +14,16 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.ATLAS_URI;
-mongoose.connect(uri, { useNewUrlParser: true}
-);
+mongoose.connect(uri, { useNewUrlParser: true });
 
 const connection = mongoose.connection;
-connection.once('open', () => {
-    console.log("MongoDB database connection established successfully");
-})
+connection.once("open", () => {
+	console.log("MongoDB database connection established successfully");
+});
 
-const recipesRouter = require('./routes/recipe');
-const usersRouter = require('./routes/users');
-const likesRouter = require('./routes/like');
-const shoppinglistRouter = require('./routes/shoppinglist');
-
-app.use('/recipe', recipesRouter);
-app.use('/users', usersRouter);
-app.use('/like', likesRouter);
-app.use('/shoppinglist', shoppinglistRouter);
+app.use("/recipe", recipesRouter);
+app.use("/quote", quoteController);
 
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+	console.log(`Server is running on port: ${port}`);
 });
